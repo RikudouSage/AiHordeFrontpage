c6cd63c7f91fb132b252d8241c60cce9b645f8e7
Dominik Chrastecky <dominik@chrastecky.cz>
2024-06-02 11:48:27 +0200

    Refresh stats every minute

--- src/app/pages/homepage/parts/stats/homepage-stats.component.ts
+++ src/app/pages/homepage/parts/stats/homepage-stats.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, signal} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID, signal} from '@angular/core';
 import {CutPipe} from "../../../../pipes/cut.pipe";
 import {FormatNumberPipe} from "../../../../pipes/format-number.pipe";
 import {InlineSvgComponent} from "../../../../components/inline-svg/inline-svg.component";
@@ -6,12 +6,13 @@ import {ShiftDecimalsLeftPipe} from "../../../../pipes/shift-decimals-left.pipe"
 import {SiPrefixPipe} from "../../../../pipes/si-prefix.pipe";
 import {TranslocoMarkupComponent} from "ngx-transloco-markup";
 import {TranslocoPipe} from "@jsverse/transloco";
-import {UpperCasePipe} from "@angular/common";
+import {isPlatformBrowser, UpperCasePipe} from "@angular/common";
 import {HordePerformance} from "../../../../types/horde-performance";
 import {SingleImageStatPoint} from "../../../../types/single-image-stat-point";
 import {SingleTextStatPoint} from "../../../../types/single-text-stat-point";
 import {AiHordeService} from "../../../../services/ai-horde.service";
 import {toPromise} from "../../../../types/resolvable";
+import {combineLatest, combineLatestWith, interval, startWith, zip} from "rxjs";
 
 @Component({
   selector: 'app-homepage-stats',
@@ -30,18 +31,34 @@ import {toPromise} from "../../../../types/resolvable";
   styleUrl: './homepage-stats.component.scss'
 })
 export class HomepageStatsComponent implements OnInit {
+  private readonly isBrowser: boolean;
+
   public stats = signal<HordePerformance | null>(null);
   public imageStats = signal<SingleImageStatPoint | null>(null);
   public textStats = signal<SingleTextStatPoint | null>(null);
 
   constructor(
     private readonly aiHorde: AiHordeService,
+    @Inject(PLATFORM_ID) platformId: string,
   ) {
+    this.isBrowser = isPlatformBrowser(platformId);
   }
 
   public async ngOnInit(): Promise<void> {
-    this.stats.set(await toPromise(this.aiHorde.performance));
-    this.imageStats.set((await toPromise(this.aiHorde.imageStats)).total);
-    this.textStats.set((await toPromise(this.aiHorde.textStats)).total);
+    if (this.isBrowser) {
+      interval(60_000).pipe(
+        startWith(0),
+        combineLatestWith(this.aiHorde.performance, this.aiHorde.imageStats, this.aiHorde.textStats)
+      ).subscribe(value => {
+        console.log(value);
+        this.stats.set(value[1]);
+        this.imageStats.set(value[2].total);
+        this.textStats.set(value[3].total);
+      });
+    } else {
+      this.stats.set(await toPromise(this.aiHorde.performance));
+      this.imageStats.set((await toPromise(this.aiHorde.imageStats)).total);
+      this.textStats.set((await toPromise(this.aiHorde.textStats)).total);
+    }
   }
 }
