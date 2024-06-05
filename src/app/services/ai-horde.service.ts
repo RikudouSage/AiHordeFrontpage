8542b8260ab514a8eb5cdafc80ec2c2e191112de
tazlin <tazlin.on.github@gmail.com>
2024-06-05 18:19:14 -0400

    fix: use live stats

--- src/app/services/ai-horde.service.ts
+++ src/app/services/ai-horde.service.ts
@@ -17,25 +17,25 @@ export class AiHordeService {
   }
 
   public get imageStats(): Observable<ImageTotalStats> {
-    return of({
-      month: { images: 105150339, ps: 1553239485353984 },
-      total: { images: 105150339, ps: 1553239485353984 },
-      day: { images: 105150339, ps: 1553239485353984 },
-      hour: { images: 105150339, ps: 1553239485353984 },
-      minute: { images: 105150339, ps: 1553239485353984 },
-    });
-    // return this.httpClient.get<TotalStats>('https://aihorde.net/api/v2/stats/img/totals');
+    // return of({
+    //   month: { images: 105150339, ps: 1553239485353984 },
+    //   total: { images: 105150339, ps: 1553239485353984 },
+    //   day: { images: 105150339, ps: 1553239485353984 },
+    //   hour: { images: 105150339, ps: 1553239485353984 },
+    //   minute: { images: 105150339, ps: 1553239485353984 },
+    // });
+    return this.httpClient.get<ImageTotalStats>('https://aihorde.net/api/v2/stats/img/totals');
   }
 
   public get textStats(): Observable<TextTotalStats> {
-    return of({
-      total: { requests: 111931745, tokens: 20444501084 },
-      day: { requests: 111931745, tokens: 20444501084 },
-      hour: { requests: 111931745, tokens: 20444501084 },
-      minute: { requests: 111931745, tokens: 20444501084 },
-      month: { requests: 111931745, tokens: 20444501084 },
-    });
-    // return this.httpClient.get<TextTotalStats>('https://aihorde.net/api/v2/stats/text/totals');
+    // return of({
+    //   total: { requests: 111931745, tokens: 20444501084 },
+    //   day: { requests: 111931745, tokens: 20444501084 },
+    //   hour: { requests: 111931745, tokens: 20444501084 },
+    //   minute: { requests: 111931745, tokens: 20444501084 },
+    //   month: { requests: 111931745, tokens: 20444501084 },
+    // });
+    return this.httpClient.get<TextTotalStats>('https://aihorde.net/api/v2/stats/text/totals');
   }
 
   public get performance(): Observable<HordePerformance> {
