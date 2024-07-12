fcc6fdd7a3c8c331623f7b32781eb4f7796d6fec
Dominik Chrastecky <dominik@chrastecky.cz>
2024-07-12 13:53:22 +0200

    Feat: Switch to documents endpoint for privacy policy

--- src/app/pages/privacy/privacy.component.ts
+++ src/app/pages/privacy/privacy.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {DataService} from "../../services/data.service";
 import {toPromise} from "../../types/resolvable";
 import {toSignal} from "@angular/core/rxjs-interop";
@@ -10,6 +10,7 @@ import {NoSorterKeyValue} from "../../types/no-sorter-key-value";
 import {ReplacePipe} from "../../pipes/replace.pipe";
 import {TranslatorService} from "../../services/translator.service";
 import {Title} from "@angular/platform-browser";
+import {AiHordeService} from "../../services/ai-horde.service";
 
 @Component({
   selector: 'app-privacy',
@@ -25,18 +26,19 @@ import {Title} from "@angular/platform-browser";
 })
 export class PrivacyComponent implements OnInit {
   protected readonly NoSorterKeyValue = NoSorterKeyValue;
-  public policyItems = toSignal(this.dataService.privacyPolicy);
+  public policy = signal<string | null>(null);
 
   constructor(
-    private readonly dataService: DataService,
     private readonly footerColor: FooterColorService,
     private readonly translator: TranslatorService,
     private readonly titleService: Title,
+    private readonly aiHorde: AiHordeService,
   ) {
   }
 
   public async ngOnInit(): Promise<void> {
     this.footerColor.dark.set(true);
+    this.policy.set(await toPromise(this.aiHorde.privacyPolicy));
     this.titleService.setTitle(await toPromise(this.translator.get('privacy_policy')));
   }
 }
